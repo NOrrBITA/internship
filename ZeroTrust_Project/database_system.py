dde994e8b92508127d28eb66a4b5f5e2bbb96fe9
NOrrBITA <nobitaop2601@gmail.com>
2026-01-01 14:29:27 +0530

    Delete ZeroTrust_Project directory

--- ZeroTrust_Project/database_system.py
+++
@@ -1,5 +0,0 @@
-def access_vault(mfa_token):
-    if mfa_token == "AUTH_TOKEN_99":
-        return "SUCCESS: Access Granted. Sensitive Data: [Project_Alpha_Files]"
-    else:
-        return "FAILURE: Access Denied. Identity could not be verified."
